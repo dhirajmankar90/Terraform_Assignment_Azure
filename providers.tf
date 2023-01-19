950bea7e390fb4e396d4a1f32b4786719afe5dba
dhirajmankar90 <101197845+dhirajmankar90@users.noreply.github.com>
2023-01-19 22:48:48 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
-# terraform {
 
-
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "3.39.1"
-#     }
-#   }
-# }
 
 provider "azurerm" {
   features {}
