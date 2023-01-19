ea492ffcd50130ff022eddefa67fee75637f7c9f
dhirajmankar90 <101197845+dhirajmankar90@users.noreply.github.com>
2023-01-19 15:04:41 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,8 +12,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  subscription_id = "7fcfbbab-6c02-4dc7-99d4-0ad01473636c"
+  tenant_id       = "b0fb9891-78ad-46b4-98ad-6b22271c7ae3"
+  client_id       = "b6fa71fb-a72e-4229-aa2b-93d51a275529"
+  client_secret   = "oGC8Q~HqJJRepp3HRvTCWdV6ASTBvVQrP405nbbT"
 }
